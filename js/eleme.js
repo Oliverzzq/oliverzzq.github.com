1c38b5e7e05e668922f4b894c21178c77165d650
Oliverzzq <522872757@qq.com>
2016-06-08 10:22:35 +0800

    add

--- js/eleme.js
+++ js/eleme.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
-$("i").click(function(){
-	if($("#place-frame").css("display")=='none'){
-        $("#triangle").css("display","inline");
-	    $("#place-frame").css("display","inline");
-	}
-	else{
-        $("#triangle").css("display","none");
-	    $("#place-frame").css("display","none");    
-	}
+	$("i").click(function(){
+		if($("#place-frame").css("display")=='none'){
+	        $("#triangle").css("display","inline");
+		    $("#place-frame").css("display","inline");
+		}
+		else{
+	        $("#triangle").css("display","none");
+		    $("#place-frame").css("display","none");    
+		}
 
-});
-$(".name").click(function(){
-	var text=$(this).html();
-	$("#place").html(text);
-    $("#triangle").css("display","none");
-	$("#place-frame").css("display","none");
-});
-$("#guess-text2").click(function(){
-	var text=$(this).html();
-	$("#place").html(text);
-    $("#triangle").css("display","none");
-	$("#place-frame").css("display","none");
-})
+	});
+	$(".name").click(function(){
+		var text=$(this).html();
+		$("#place").html(text);
+	    $("#triangle").css("display","none");
+		$("#place-frame").css("display","none");
+	});
+	$("#guess-text2").click(function(){
+		var text=$(this).html();
+		$("#place").html(text);
+	    $("#triangle").css("display","none");
+		$("#place-frame").css("display","none");
+	});
 });
